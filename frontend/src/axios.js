a59a493de476eea8cde4a270f1bdb5c4fb5a6096
Uday032 <rahul.uday07@gmail.com>
2021-02-16 23:20:00 +0530

    news setuped

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -8,7 +8,11 @@ const coingekoinstance = axios.create({
     baseURL: "https://api.coingecko.com/api/v3"
 });
 
+const newsapi = axios.create({
+    baseURL: "https://newsapi.org/v2"
+})
 export { 
     instance, 
-    coingekoinstance
+    coingekoinstance,
+    newsapi
 };
--- frontend/src/components/NewsHeadlineCard.js
+++ frontend/src/components/NewsHeadlineCard.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
 import {Card} from 'react-bootstrap'
-import logo from '../logo.svg'
+
 
 export default class NewsHeadlineCard extends Component {
     render() {
         return (
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={logo} />
+            <Card>
+                <Card.Img variant="top" src={this.props.image} />
                 <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
+                <Card.Title>{this.props.title}</Card.Title>
+                <Card.Text>
+                    {this.props.description}
+                    <a href={this.props.redirecturl} target="_blank">view more...</a>
+                </Card.Text>
                 </Card.Body>
+                <Card.Footer>
+                <small className="text-muted">Posted by {this.props.author}</small>
+                </Card.Footer>
             </Card>
         )
     }
